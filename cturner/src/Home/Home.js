4af0272c28695a45034902130a27e7b5c5494865
Cameron Turner <cameron.turner.2014@uni.strath.ac.uk>
2019-06-08 12:26:35 +0100

    Updated querystringify package.

--- cturner/src/Home/Home.js
+++ cturner/src/Home/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
-import { Header } from "semantic-ui-react";
+import { Header, Image, Container, Grid } from "semantic-ui-react";
+import picture from "../Images/landscape.jpg";
 
 export class Home extends Component {
     render () {
         return (
-            <Header as="h1">Home</Header>
+            <div>
+                <Header as="h1" >Home</Header>
+                <Container>
+                    <Image src={picture} fluid />
+                </Container>
+            </div>
         );
     }
 }
